5ec778df7058876d970588e507b99033994c8ff8
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2025-04-24 22:30:59 -0300

    Remove menu

--- frontend/src/models/itemDTO.js
+++ frontend/src/models/itemDTO.js
@@ -1,12 +1,12 @@
 class ItemDTO {
-    constructor({nombre, descripcion, precio, categoriaId, tag, imagen}) {
+    constructor({nombre, descripcion, precio, categoriaId, tag, imageURL}) {
         this.nombre = String(nombre);
         this.descripcion = String(descripcion);
         this.precio = Number(precio);
         this.categoriaId = String(categoriaId); 
         this.stock = true; //por default disponible
         this.tag = String(tag); 
-        this.imagen = imagen; //todo: ver que onda
+        this.imagen = imageURL; //todo: ver que onda
     }
     static fromJson(data) { 
         return new ItemDTO(data); 
